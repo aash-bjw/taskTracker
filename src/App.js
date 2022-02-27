6be985834bafe2701feec29f94b58b2766af5a12
aash-bjw <50807447+aash-bjw@users.noreply.github.com>
2022-02-27 18:36:15 -0500

    adding delete & reminders

--- src/App.js
+++ src/App.js
@@ -1,9 +1,46 @@
+import { useState } from 'react'
 import Header from './components/Header'
+import Tasks from './components/Tasks'
+
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: 'Grocery Shopping',
+        day: 'Feb 27th at 6:30pm',
+        reminder: false
+    },
+    {
+        id: 2,
+        text: 'Oil Change',
+        day: 'March 7th at 3:30pm',
+        reminder: true
+    },
+    {
+        id: 3,
+        text: 'Pup Grooming',
+        day: 'March 5th at 1:30pm',
+        reminder: true
+    },
+
+])
+
+//Delete Task
+const deleteTask = (id) => {
+  setTasks(tasks.filter((task) => task.id !== id))
+}
+
+//Toggle Reminder
+  const toggleReminder = (id) => {
+    setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder } : task))
+  }
+
+
   return (
     <div className='container'>
-      <Header title = 'Hello'/>
+      <Header />
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : 'No Tasks To Show'}
     </div>
   );
 }
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,31 +1,15 @@
+import Task from './Task'
 
-const tasks = [
-    {
-        id: 1,
-        text: 'Grocery Shopping',
-        day: 'Feb 27th at 6:30pm',
-        reminder: false
-    },
-    {
-        id: 2,
-        text: 'Oil Change',
-        day: 'March 7th at 3:30pm',
-        reminder: true
-    },
-    {
-        id: 3,
-        text: 'Pup Grooming',
-        day: 'March 5th at 1:30pm',
-        reminder: true
-    },
-
-]
-
-function Tasks() {
+const Tasks = ({ tasks, onDelete, onToggle }) => {
     return (
         <>
-            {tasks.map((tasks) => (
-                <h3>{tasks.text}</h3>
+            {tasks.map((task) => (
+                <Task
+                    key={tasks.id}
+                    task={task}
+                    onDelete={onDelete}
+                    onToggle={onToggle}
+                />
             ))}
         </>
     )
